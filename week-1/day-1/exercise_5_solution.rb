fcc264395f416b2426a07c385525edd38547f840
Nikola Novakovic <nikola.novakovic9@gmail.com>
2017-10-26 16:33:59 -0400

    exercise 5 solution

---
+++ week-1/day-1/exercise_5_solution.rb
@@ -0,0 +1,14 @@
+puts "Item 1: "
+first_item = gets.chomp
+
+puts "Item 1 - Price: "
+first_item_price = gets.chomp
+
+puts "Item 2: "
+second_item = gets.chomp
+
+puts "Item 2 - Price: "
+second_item_price = gets.chomp
+
+output = "#{first_item} - #{first_item_price}$\n#{second_item} - #{second_item_price}$"
+puts output
