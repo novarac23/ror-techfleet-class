c9f1caa389495604e832c4e3efa90163b044c5ca
Nikola Novakovic <nikola.novakovic9@gmail.com>
2017-10-28 09:50:28 -0400

    Finish up arrays

---
+++ week-1/day-3-non-exercise/array_samples.rb
@@ -0,0 +1,16 @@
+class Stuff
+  def self.print
+    puts  "Hello there mate"
+  end
+end
+
+
+numbers = [1, 2, 3, 4, 5]
+numbers_second_version = [1..50]
+numbers_third_version = [50..1]
+
+strings = ["ruby", "c", "java", "elixir"]
+
+array_of_arrays = [[1,2], [2,3], [4,5]]
+
+array_of_arrays = [["FIZBUZZ",2], [Stuff.print,3], [4,5]]
