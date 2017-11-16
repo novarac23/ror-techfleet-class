152b4d5f1b738e912d2863e1b536c465c9411b44
Nikola Novakovic <nikola.novakovic9@gmail.com>
2017-11-16 08:36:53 -0500

    Update file exercise

--- week-4/day-2/exercise_1_solution.rb
+++ week-4/day-2/exercise_1_solution.rb
@@ -2,7 +2,9 @@ class IceCreamReader
   def self.print
     File.open("./ice_creams.txt", "r") do |f|
       f.each_line do |line|
-        puts "#{line}"
+        line.split.each do |word|
+          puts "#{word}"
+        end
       end
     end
   end
